e27589b58349ed9c8abf8b3041a5dde9bf09a9bc
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-02-26 14:33:37 -0600

    Modelo de producto

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+class Product{
+    constructor(id, title, description, images, price){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.images = images;
+        this.price = price;
+    }
+}

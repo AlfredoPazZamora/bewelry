bbf90546fa253a112d9c55fd8dfb7ec71db67907
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-02-26 15:03:10 -0600

    Agregamos categoria al producto como dato

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,10 @@
 class Product{
-    constructor(id, title, description, images, price){
+    constructor(id, title, description, images, price, category){
         this.id = id;
         this.title = title;
         this.description = description;
         this.images = images;
         this.price = price;
+        this.category = category;
     }
 }

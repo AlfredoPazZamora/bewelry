8a6b0b675386e4a10233a088012d3664f0dfcbed
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-02-28 22:00:56 -0600

    Configuración básica de app

---
+++ app.js
@@ -0,0 +1,29 @@
+//Importación de las bibliotecas
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
+
+//Objeto global
+let app = express();
+
+//Configuración de middelwares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Rutas
+
+
+//Manejando los errores
+app.use((req, res, next) => {
+    let err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+//Iniciando el servidor
+let port = process.env.PORT || 3000;
+let server = app.listen(port, () => {
+    console.log('Escuchando en el puerto' + server.address().port );
+});
+    

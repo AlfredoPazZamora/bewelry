4605e6a44711e6b41bc9df6fcd27446ea2dc089e
Jose Juarez <intjose_123@hotmail.com>
2021-02-28 23:54:25 -0600

    Cambio de nombre de funciones y llenado de funciones

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
 class Product{
-    constructor(id, title, description, images, price, category){
+    constructor(id, title, description, images, price, category,owner){
         this.id = id;
         this.title = title;
         this.description = description;
         this.images = images;
         this.price = price;
         this.category = category;
+        this.owner = owner;
     }
 }

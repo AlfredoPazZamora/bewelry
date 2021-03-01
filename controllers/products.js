04b44c46822480501e50208fede23cca67e85465
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-02-28 22:09:45 -0600

    Creacion de controlador de producto

---
+++ controllers/products.js
@@ -0,0 +1,24 @@
+//Importamos el modelo
+const Product = require('../models/Product');
+
+//Funcionalidades
+function mostrarArticulos(req, res){};
+
+function buscarArticulos(req, res){};
+
+function publicarArticulo(req, res){};
+
+function actualizarArticulos(req, res){};
+
+function eliminarArticulos(req, res){};
+
+function verArticulosPorUsuario(req, res){};
+
+module.exports = {
+    mostrarArticulos,
+    buscarArticulos,
+    publicarArticulo,
+    actualizarArticulos,
+    eliminarArticulos,
+    verArticulosPorUsuario
+};

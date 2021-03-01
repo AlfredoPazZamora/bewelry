6cc6873f366117a296a4913d12559691b8c47d36
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-02-28 22:06:56 -0600

    Creacion de controlador de usuario

---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+//Importamos el modelo
+const User = require('../models/User');
+
+//Funcionalidades
+function verDatosPersonales(req, res){};
+
+function actualizarDatosPersonales(req, res){}
+
+module.exports = {
+    verDatosPersonales,
+    actualizarDatosPersonales
+};
+
